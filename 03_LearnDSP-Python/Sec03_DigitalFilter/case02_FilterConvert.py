3d016309702977853dcc89a993b68730897f5262
Chunyu <chunyu2018@foxmail.com>
2021-02-07 21:41:16 +0800

    Manully realize 'Impulse invariance method'

--- 03_LearnDSP-Python/Sec03_DigitalFilter/case02_FilterConvert.py
+++ 03_LearnDSP-Python/Sec03_DigitalFilter/case02_FilterConvert.py
@@ -10,7 +10,6 @@ XiaoCY 2021-02-07
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.signal as sig
-import PyDynamic.misc as misc 
 
 fs = 10.0                                           # sampling frequency (Hz)
 fc = 1.0                                            # corner frequency (Hz)
@@ -18,7 +17,16 @@ b,a = sig.butter(2,2*np.pi*fc,analog=True)          # demo transfer function to 
 
 # %% Filter conversion
 # Impulse invariance method
+r,p,k = sig.residue(b,a)
+p = np.exp(p/fs)
+r = r/fs
+bz1,az1 = sig.invresz(r,p,k)
+bz1 = bz1.real
+az1 = az1.real
+''' Another way
+import PyDynamic.misc as misc 
 bz1,az1 = misc.impinvar(b,a,fs=fs)
+'''
 
 # Bilinear transformation method
 bz2,az2 = sig.bilinear(b,a,fs=fs)
