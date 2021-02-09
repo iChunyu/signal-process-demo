2477444819dc7687288c44ac75cf7e4c2fb11989
Chunyu <chunyu2018@foxmail.com>
2021-02-09 10:54:41 +0800

    GUI tool to design filter

---
+++ 03_LearnDSP-Python/Sec03_DigitalFilter/case05_FilterGUI.py
@@ -0,0 +1,14 @@
+'''
+Design filter with Graphical User Interface
+No more description, just follow the GUI steps
+
+XiaoCY 2021-02-09
+'''
+
+#%%
+'''
+The GUI designer depends on `pyfda`, install it at first:
+    pip install pyfda
+'''
+
+!pyfdax             # call pyfda tool through terminal
