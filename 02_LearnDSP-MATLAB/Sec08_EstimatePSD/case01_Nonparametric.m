e6a67aa4f254cf8900b99b1cf1a5805aaf6b5988
Chunyu <chunyu2018@foxmail.com>
2021-02-24 11:36:08 +0800

    Nonparametric PSD estimation

---
+++ 02_LearnDSP-MATLAB/Sec08_EstimatePSD/case01_Nonparametric.m
@@ -0,0 +1,43 @@
+% Estimate PSD with nonparametric methods
+
+% XiaoCY 2021-02-24
+
+%%
+clear;clc
+
+fs = 1000;                  % sampling frequency (Hz)
+Wp = 30/(fs/2);             % passband corner frequency (normalized)
+Ws = 50/(fs/2);             % stopband corner frequency (normalized)
+Rp = 1;                     % passband ripple (dB)
+Rs = 40;                    % stopband attenuation (dB)
+
+[n,Wn] = cheb2ord(Wp,Ws,Rp,Rs);
+[b,a] = cheby2(n,Rs,Wn);
+x = randn(round(10*fs),1)*sqrt(fs/2);
+x = filter(b,a,x);
+
+%% periodogram
+N = length(x);
+nfft = N;
+win = hanning(N);
+[px1,f1] = periodogram(x,win,nfft,fs,'onesided');
+
+%% pmtm
+[px2,f2] = pmtm(x,4,N,fs,'onesided');
+
+%% pwelch
+nfft = round(N/5);
+win = hanning(nfft);
+noverlap = round(nfft*0.6);
+[px3,f3] = pwelch(x,win,noverlap,nfft,fs,'onesided');
+
+%% results
+figure
+loglog(f1,px1,'DisplayName','periodogram')
+hold on
+grid on
+loglog(f2,px2,'DisplayName','pmtm')
+loglog(f3,px3,'DisplayName','pwelch')
+legend
+xlabel('Frequency (Hz)')
+ylabel('PSD')
